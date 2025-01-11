46cb1c428cdd484c2c051ba4cbf26e87215afd0c
challengeable <udelu@hotmail.com>
2025-01-11 00:33:29 -0500

    loading states

--- src/contentScript/[playlist].ts
+++ src/contentScript/[playlist].ts
@@ -15,7 +15,8 @@ const observer = new MutationObserver(async (mutationsList) => {
             if (mutation.target instanceof HTMLElement) {
                 if (mutation.target.hasAttribute('yt-retrieved')) {
                     if (mutation.target.getAttribute('src').includes('no_thumbnail.jpg')) {
-                        mutation.target.setAttribute('src', 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-560w,f_auto,q_auto:best/newscms/2019_06/2746941/190208-stock-money-fanned-out-ew-317p.jpg');
+                        let loading_image_src: string = chrome.runtime.getURL('/img/loading_states/loading_state_logo.png');
+                        mutation.target.setAttribute('src', loading_image_src);
 
                         const ytImageElement: Element = mutation.target?.parentElement;
 
@@ -48,8 +49,7 @@ const observer = new MutationObserver(async (mutationsList) => {
 
                                             populateVideo(metadata_res[0], mutation.target as Element);
                                         } else {
-                                            console.log(raw_watch_url);
-                                            console.log(video_id);
+                                            markVideoAsNulledMetadata(mutation.target as Element);
                                         }
 
                                     })
@@ -114,8 +114,9 @@ export const getThumbnails = async () => {
         const thumbnail_image = anchor.getElementsByTagName('img')[0];
         if (thumbnail_image) {
             if (!thumbnail_image?.src || thumbnail_image?.src.includes('no_thumbnail.jpg')) {
+                let loading_image_src: string = chrome.runtime.getURL('/img/loading_states/loading_state_logo.png');
                 //@ts-ignore
-                thumbnail_image.setAttribute('src', 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-560w,f_auto,q_auto:best/newscms/2019_06/2746941/190208-stock-money-fanned-out-ew-317p.jpg');
+                thumbnail_image.setAttribute('src', loading_image_src);
                 thumbnail_image.setAttribute('yt-retrieved', 'true');
             }
         }
@@ -136,16 +137,16 @@ const populateVideo = async (metadata: FilmotMetadataResponseDTO, element: Eleme
             if (href_attr && href_attr.includes(`/watch?v=${metadata.id}`)) {
                 ancEle.setAttribute('href', `https://www.youtube.com/results?search_query=${metadata.title}`)
             }
-            
+
         }
 
         const title: Element = root_parent.querySelector('#video-title');
-        title.textContent = `[📦] ${metadata.title}`;
+        title.textContent = `🐕📼 ${metadata.title}`;
 
         const meta_container: Element = root_parent.querySelector('#meta');
 
         const recovered_by_div = document.createElement('div');
-        recovered_by_div.style.color = '#aaa';
+        recovered_by_div.style.color = '#f5dbb6';
         recovered_by_div.style.fontSize = '1.2rem';
         recovered_by_div.style.fontFamily = '"Roboto","Arial",sans-serif'
         recovered_by_div.innerText = `Recovered by YT-Retriever`;
@@ -167,6 +168,29 @@ const populateVideo = async (metadata: FilmotMetadataResponseDTO, element: Eleme
         meta_container.appendChild(recovered_by_div);
         meta_container.appendChild(video_info_div);
 
+        let loaded_image_src: string = chrome.runtime.getURL('/img/loading_states/loaded_state_logo.png');
+        element.setAttribute('src', loaded_image_src);
+
+    }
+
+}
+
+const markVideoAsNulledMetadata = (element: Element) => {
+    let loaded_image_src: string = chrome.runtime.getURL('/img/loading_states/failed_state_logo.png');
+    element.setAttribute('src', loaded_image_src);
+
+    const root_parent: Element = element.closest('#content');
+
+    if (root_parent) {
+        const meta_container: Element = root_parent.querySelector('#meta');
+
+        const could_not_find = document.createElement('div');
+        could_not_find.style.color = '#f5dbb6';
+        could_not_find.style.fontSize = '1.2rem';
+        could_not_find.style.fontFamily = '"Roboto","Arial",sans-serif'
+        could_not_find.innerText = `🐾 I've woofed everywhere and could not find this video.. sowy!`;
+
+        meta_container.appendChild(could_not_find);
     }
 
 }
